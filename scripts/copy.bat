c7175c2fd810578a90512a6fab9269e24d242698
Marco Koellner <it@mk-swe.de>
2019-10-20 21:17:22 +0200

    Update tool popper

--- scripts/copy.bat
+++ scripts/copy.bat
@@ -2,9 +2,9 @@ rem npm ls
 rem npm outdated
 call npm update
 
-robocopy node_modules\bootstrap\dist kirby\assets\vendors\bootstrap /MIR
-robocopy node_modules\jquery\dist kirby\assets\vendors\jquery /MIR
-robocopy node_modules\popper.js\dist kirby\assets\vendors\popper.js /MIR
-robocopy node_modules\@fortawesome\fontawesome-free kirby\assets\vendors\fontawesome-free /MIR
-robocopy node_modules\cookieconsent\src kirby\assets\vendors\cookieconsent /MIR
-robocopy node_modules\photoswipe\dist kirby\assets\vendors\photoswipe /MIR
+robocopy node_modules\bootstrap\dist assets\vendors\bootstrap /MIR
+robocopy node_modules\jquery\dist assets\vendors\jquery /MIR
+robocopy node_modules\popper.js\dist assets\vendors\popper.js /MIR
+robocopy node_modules\@fortawesome\fontawesome-free assets\vendors\fontawesome-free /MIR
+robocopy node_modules\cookieconsent\src assets\vendors\cookieconsent /MIR
+robocopy node_modules\photoswipe\dist assets\vendors\photoswipe /MIR
