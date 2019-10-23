51def3e987317a1505fa6ef455e3c0e613183ca1
Marco Koellner <it@mk-swe.de>
2019-10-23 22:12:31 +0200

    Umbau Termine; Weiterfuehrende Links & Warnung

--- site/snippets/termine.php
+++ site/snippets/termine.php
@@ -13,15 +13,20 @@ $items = $page->dates()->toStructure(); ?>
   <h4><span class="badge badge-secondary"><?= $item->date()->toDate('d') ?></span></h4>
   <h5><?= $item->date()->toDate('M - Y') ?><h5>
   </div>
-  <div class="col-10">
-  <h5 class="text-uppercase text-left"><?= $item->title()->html() ?> <?= e($item->public()->isTrue(), '<a href="#" class="btn btn-success ml-5 btn-large">Tickets</a>') ?></h5>
-  <ul class="list-inline text-justify">
-        <li class="list-inline-item"><i class="far fa-calendar mr-2" aria-hidden="true"></i><?= $item->date()->toDate('l') ?></li>
-        <li class="list-inline-item"><i class="far fa-clock mr-2" aria-hidden="true"></i><?= $item->time()->toDate('H:i') ?></li>
-        <li class="list-inline-item"><i class="fas fa-map-marker mr-2" aria-hidden="true"></i><?= $item->place() ?></li>
-        <?= e($item->public()->isTrue(), '<li class="badge badge-success badge-pill">Öffentlich</li>') ?>
+  <div class="col">
+    <h5 class="text-uppercase text-left"><?= $item->title()->html() ?></h5>
+    <ul class="list-inline text-justify">
+            <li class="list-inline-item"><i class="far fa-calendar mr-2" aria-hidden="true"></i><?= $item->date()->toDate('l') ?></li>
+            <li class="list-inline-item"><i class="far fa-clock mr-2" aria-hidden="true"></i><?= $item->time()->toDate('H:i') ?></li>
+            <li class="list-inline-item"><i class="fas fa-map-marker mr-2" aria-hidden="true"></i><?= $item->place() ?></li>
+            <?= e($item->public()->isTrue(), '<li class="badge badge-success badge-pill">Öffentlich</li>') ?>
     </ul>
-    </div>
+  </div>
+  <?php if($item->target()->isNotEmpty()): ?>
+  <div class="col-3">
+    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#datesModal"><?= $item->targettext()->html()?><?php $item->targettext() ?></button>
+  </div>
+  <?php endif ?>
   </div>
 
   
@@ -31,5 +36,22 @@ $items = $page->dates()->toStructure(); ?>
 <?php endforeach ?>
 </ul>
 
-
+<div class="modal fade" id="datesModal" tabindex="-1" role="dialog" aria-labelledby="datesModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content bg-info text-light">
+      <div class="modal-header">
+        <h5 class="modal-title" id="datesModalLabel">Kartenvorverkauf</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="container">
+        <p><strong>Kartenvorverkauf Prunksitzungen und Rosenmontagsball!</strong></p>
+<p>Sie haben Interesse an Eintrittskarten für unsere Veranstaltungen?</p>
+<p>Ab Mittwoch, den <strong>09.12.2019</strong>, sind hier unsere Tickets verfügbar.</p>    </div>
+      </div>
+    </div>
+  </div>
+</div>
 </div>
