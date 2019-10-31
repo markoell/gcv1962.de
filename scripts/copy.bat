8668b03f8b5cefb7adca7d3d8b0ba8f9181a49c5
Marco Köllner <koellner@tmn-systemberatung.de>
2019-10-31 14:48:08 +0100

    Waypoint FW hinzugefügt; Script.js erstellt

--- scripts/copy.bat
+++ scripts/copy.bat
@@ -8,3 +8,4 @@ robocopy node_modules\popper.js\dist assets\vendors\popper.js /MIR
 robocopy node_modules\@fortawesome\fontawesome-free assets\vendors\fontawesome-free /MIR
 robocopy node_modules\cookieconsent\src assets\vendors\cookieconsent /MIR
 robocopy node_modules\photoswipe\dist assets\vendors\photoswipe /MIR
+robocopy node_modules\waypoints\lib assets\vendors\waypoints /MIR
