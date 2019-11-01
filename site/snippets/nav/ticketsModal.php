373dd088fa7206c41b7057f42aa39cd008612817
Marco Koellner <it@mk-swe.de>
2019-11-01 21:11:24 +0100

    Bugfix des Modalen-Ticket-Fenster

---
+++ site/snippets/nav/ticketsModal.php
@@ -0,0 +1,21 @@
+<?php $tmp = page('home/Termine') ?>
+<div class="modal fade" id="modalTickets" tabindex="-1" role="dialog" aria-labelledby="modalTicketsTitle"
+  aria-hidden="true">
+  <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalTicketsTitle"><?= $tmp->title()->html() ?>
+        </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <?php snippet('termine', array('dates' => $tmp->dates()->toStructure()->filterBy('targettext', 'Tickets'))) ?>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success" data-dismiss="modal">Schließen</button>
+      </div>
+    </div>
+  </div>
+</div>
