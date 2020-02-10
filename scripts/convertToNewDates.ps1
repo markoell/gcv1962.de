0cfd1a79481a682c811ff70fbb1aea168e170f50
Marco Koellner <it@mk-swe.de>
2020-02-10 21:47:53 +0100

    New blueprints for dates:
    * Template for single date
    
    * Template for dates overview
    
    * script draft to convert old dates to new ones

---
+++ scripts/convertToNewDates.ps1
@@ -0,0 +1,31 @@
+$template = '
+Title: {Title}
+
+----
+
+From: {From:2020-02-10 00:00}
+
+----
+
+Completeday: {completeDay}
+
+----
+
+Location: {Location}
+
+----
+
+Link: {Url}
+
+----
+
+Text: {Text}
+
+----
+
+Categories: {Categories}
+'
+
+$title = "";
+
+$template.Replace('{Title}', $title);
