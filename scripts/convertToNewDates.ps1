d8f851e3a9ae31484a30d93a7d684355ca0e988f
Marco Koellner <it@mk-swe.de>
2020-02-12 22:44:33 +0100

    Script zum Konvertieren alter Termine ins neue Format

--- scripts/convertToNewDates.ps1
+++ scripts/convertToNewDates.ps1
@@ -1,9 +1,8 @@
-$template = '
-Title: {Title}
+$template = 'Title: {Title}
 
 ----
 
-From: {From:2020-02-10 00:00}
+From: {From}
 
 ----
 
@@ -23,9 +22,50 @@ Text: {Text}
 
 ----
 
-Categories: {Categories}
-'
+Categories: {Categories}'
 
-$title = "";
+$content = Get-Content -Path .\termine.txt
+$title = ""
+$date = ""
+$time = "00:00"
+$place = ""
+$public = ""
+$link = ""
+$target = ""
 
-$template.Replace('{Title}', $title);
+$content | ForEach-Object {
+  if($_ -eq "- " -and $title -ne ""){
+    $complete = $(if($time -eq "00:00" -or $title -eq "") { "true" } else { "false" })
+    $categories = @()
+    if($public -eq "true") { $categories += "public" }
+    if($target -eq "Tickets") { $categories += "buy" }
+    $folder = New-Item -ItemType Directory -Path ..\..\termine\_drafts\ -Name ($title.Replace("- ","-").Replace(" ","-").Replace(".",""))
+    $file = New-Item -ItemType File -Path $folder.FullName -Name "termin.txt"
+    $template.Replace("{Title}",$title).Replace("{From}","$date $time").Replace("{Location}", $place).Replace("{completeDay}",$complete).Replace("{Url}", $link).Replace("{Text}",$target).Replace("{Categories}", [string]::Join(', ', $categories)) | Out-File -Encoding utf8 $file
+  }
+  if($_ -match "\stitle\:\s'?(.*?)'?$"){
+    $title = $Matches[1]
+  }
+  if($_ -match "\sdate\:\s(.*?)$"){
+    $date = $Matches[1]
+  }
+  if($_ -match "\stime\:\s(.*?)$"){
+    $time = $Matches[1]
+  }
+  if($_ -match "\splace\:\s(.*?)$"){
+    $place = $Matches[1]
+  }
+  if($_ -match "\spublic\:\s'(.*?)'$"){
+    $public = $Matches[1]
+  }
+  if($_ -match "\s+(https\:.*)$"){
+    $link = $Matches[1]
+  }
+  if($_ -match "\stargettext\:\s(.*?)$"){
+    $target = $Matches[1]
+  }   
+}
+
+$folder = New-Item -ItemType Directory -Path ..\..\termine\_drafts\ -Name ($title.Replace("- ","-").Replace(" ","-").Replace(".",""))
+$file = New-Item -ItemType File -Path $folder.FullName -Name "termin.txt" 
+$template.Replace("{Title}",$title).Replace("{From}","$date $time").Replace("{Location}", $place).Replace("{completeDay}",$complete).Replace("{Url}", $link).Replace("{Text}",$target).Replace("{Categories}", [string]::Join(', ', $categories)) | Out-File -Encoding utf8 $file
