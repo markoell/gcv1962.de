eaf39466517b345d7a29ccbb313b832d00973e2d
Marco Koellner <it@mk-swe.de>
2020-06-28 21:02:59 +0200

    Enhance Scripts & Update Kirby $ Tools

--- scripts/Linux/Update_Submodules.sh
+++ scripts/Linux/Update_Submodules.sh
@@ -6,6 +6,11 @@
 #
 #############################################
 
+temp_dir=$(pwd)
+script_path=$(dirname $(realpath $0))
+
+cd ${script_path}/../../
+
 #update all submodules
 #git submodule update --init --recursive
 
@@ -13,7 +18,17 @@
 git submodule foreach --recursive git checkout master
 git submodule foreach --recursive git pull
 
+#--- Update Composer ------------------------
+
+composer update
+
+#--- Update NPM ------------------------
+
+npm update && /bin/bash ${script_path}/copy_npm_assets.sh
+
 #--- Checkin --------------------------------
 git add -A
 git commit -am "Kirby & Tools Update"
 #git push
+
+cd ${temp_dir}
