fdbc4a39f40ef3309a65f6508dd80482d8b763b1
Marco Koellner <it@mk-swe.de>
2020-06-29 20:34:16 +0200

    change docker build script

--- scripts/Linux/setup_docker.sh
+++ scripts/Linux/setup_docker.sh
@@ -3,7 +3,8 @@
 # !!! Ensure to remove all container with name gcv1962-webpage !!!
 
 script_path=$(dirname $(realpath $0))
+image_tag="php:7.3-apache-stretch-rewrite"
 
 #dockerize env
-docker build -t php:7.3-apache-stretch-rewrite -f ${script_path}/Dockerfile .
-docker run -d --name gcv1962-webpage -p 80:80 -v ${script_path}/../../:/var/www/html php:7.3-apache-stretch-rewrite
+docker build -t ${image_tag} -f ${script_path}/Dockerfile .
+docker run -d --name gcv1962-webpage -p 80:80 -v ${script_path}/../../:/var/www/html ${image_tag}
