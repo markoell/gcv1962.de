27736bf68e9eb72a99d8094860e164680f427ed1
Marco Koellner <it@mk-swe.de>
2020-07-23 21:37:52 +0200

    cleanup

---
+++ scripts/Linux/server_publish.sh
@@ -0,0 +1,15 @@
+# upload files to server using ssh & rsync
+#
+# Parameter: $1 -> remote connection including user, host, path;
+# Parameter: $2 -> SourceFolder
+# Parameter: $3 -> Key
+# Parameter: $4 -> KnownHost
+
+
+mkdir ~/.ssh
+echo "$4" > ~/.ssh/known_hosts
+chmod 700 ~/.ssh && chmod 644 ~/.ssh/known_hosts
+eval "$(ssh-agent -s)"
+ssh-add - <<< "$3"
+echo 'rsync -a "$2" -e ssh "$1"' # {USER}@{HOST}:{PATH}
+echo "ssh {$1:HOST} 'rm {$1:PATH}; {$1:PARENT/$2} {$1:PATH}'"
--- scripts/deploy/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#install composer libraries
-composer install
-
-#install npm libraries
-npm install && /bin/bash copy_npm_assets.sh
-
-ls -rl
