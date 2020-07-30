b453c1098b6a9ddaec80443d73efd857434d5a3d
Marco Koellner <it@mk-swe.de>
2020-07-30 22:10:09 +0200

    bugfix cleanup

--- scripts/Linux/server_publish.sh
+++ scripts/Linux/server_publish.sh
@@ -2,8 +2,9 @@
 #
 # Parameter: $1 -> remote connection including user, host, path;
 # Parameter: $2 -> SourceFolder
-# Parameter: $3 -> Key
-# Parameter: $4 -> KnownHost
+# Parameter: $3 -> TargetPath
+# Parameter: $4 -> Key
+# Parameter: $5 -> KnownHost
 
 
 mkdir ~/.ssh
@@ -12,4 +13,7 @@ chmod 700 ~/.ssh && chmod 644 ~/.ssh/known_hosts
 eval "$(ssh-agent -s)"
 ssh-add - <<< "$3"
 rsync -a "$2" -e ssh "$1" # {USER}@{HOST}:{PATH}
-echo "ssh {$1:HOST} 'rm {$1:PATH}; {$1:PARENT/$2} {$1:PATH}'"
+echo "ssh {$1%:*} 'cd ${1#*:} &&  '"
+
+# target = `basename ${1#*:}`
+# dirname = `dirname ${1#*:}`
