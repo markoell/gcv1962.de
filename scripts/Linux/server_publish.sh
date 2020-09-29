0aba748d46c32fc195ab91551c28d919f4599e25
Marco Koellner <it@mk-swe.de>
2020-09-29 21:36:52 +0200

    bugfix linux Script

--- scripts/Linux/server_publish.sh
+++ scripts/Linux/server_publish.sh
@@ -1,18 +1,17 @@
 # upload files to server using ssh & rsync
 #
-# Parameter: $1 -> remote connection including user, host, path;
+# Parameter: $1 -> remote connection including user, host, path;  (ftp273539-2688012@webhosting78.1blu.de:www/testsub)
 # Parameter: $2 -> SourceFolder
-# Parameter: $3 -> TargetPath
-# Parameter: $4 -> Key
-# Parameter: $5 -> KnownHost
+# Parameter: $3 -> Key
+# Parameter: $4 -> KnownHost
 
 
 mkdir ~/.ssh
 echo "$4" > ~/.ssh/known_hosts
 chmod 700 ~/.ssh && chmod 644 ~/.ssh/known_hosts
 eval "$(ssh-agent -s)"
-ssh-add - <<< "$3"
-rsync -a "$2" -e ssh "$1" # {USER}@{HOST}:{PATH}
+ssh-add - <<< $3
+rsync -a "$2/*" -e ssh "$1" # {USER}@{HOST}:{PATH}
 echo "ssh {$1%:*} 'cd ${1#*:} &&  '"
 
 # target = `basename ${1#*:}`
