61bdc0ccfe37bc58368d3fc24616c18334852a83
Marco Koellner <it@mk-swe.de>
2021-01-03 19:06:05 +0100

    bugfix publish

--- deployment/blue_green.sh
+++ deployment/blue_green.sh
@@ -6,9 +6,9 @@ content=${3:-"content"}
 config=${4:-"site/config"}
 accounts=${5:-"site/accounts"}
 
-rsync -a "${source}/${content%/}" "${target}/"
-rsync -a "${source}/${config%/}" "${target}/"
-rsync -a "${source}/${accounts%/}" "${target}/"
+rsync -a "${source}/${content%/}" "${target}/${content%/}"
+rsync -a "${source}/${config%/}" "${target}/${config%/}"
+rsync -a "${source}/${accounts%/}" "${target}/${accounts%/"
 
-mv -rf ${source} ${source}_`date -u --iso-8601=seconds`
+mv -f ${source} ${source}_`date -u --iso-8601=seconds`
 ln -s ${target} ${source}
