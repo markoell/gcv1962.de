bc04a17bd1b636d7f87c0afeb0da6e71423a3a80
Marco Koellner <it@mk-swe.de>
2021-01-05 18:28:17 +0100

    bugfix 102 for publish

--- deployment/blue_green.sh
+++ deployment/blue_green.sh
@@ -8,15 +8,15 @@ content=${4:-"content"}
 config=${5:-"site/config"}
 accounts=${6:-"site/accounts"}
 
-working_dir=dirname ${source_path}
-source_folder=basename ${source_path}
+working_dir=`dirname ${source_path}`
+source_folder=`basename ${source_path}`
 target_folder=`basename ${target_path}`"/${subpath}"
 
-cd working_dir
+cd ${working_dir}
 
-rsync -a "${source_folder}/${content}" "${target_folder}/${content}"
-rsync -a "${source_folder}/${config}" "${target_folder}/${config}"
-rsync -a "${source_folder}/${accounts}" "${target_folder}/${accounts}"
+rsync -a "${source_folder}/${content}/" "${target_folder}/${content}/"
+rsync -a "${source_folder}/${config}/" "${target_folder}/${config}/"
+rsync -a "${source_folder}/${accounts}/" "${target_folder}/${accounts}/"
 
 mv -f ${source_folder} ${source_folder}_`date -u --iso-8601=seconds`
 ln -s ${target_folder} ${source_folder}
