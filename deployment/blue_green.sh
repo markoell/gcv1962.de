521c2e2d241181208ac20af0a4f45c97ebceca57
Marco Koellner <it@mk-swe.de>
2021-01-05 17:49:15 +0100

    bugfix 100 for publish

--- deployment/blue_green.sh
+++ deployment/blue_green.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-source=${1%/}
-target=${2%/}
-content=${3:-"content"}
-config=${4:-"site/config"}
-accounts=${5:-"site/accounts"}
+source_path=${1%/}
+target_path=${2%/}
+subpath=${3:-"publish"}
 
-rsync -a "${source}/${content%/}/*" "${target}/${content%/}/"
-rsync -a "${source}/${config%/}/*" "${target}/${config%/}/"
-rsync -a "${source}/${accounts%/}/*" "${target}/${accounts%/}/"
+content=${4:-"content"}
+config=${5:-"site/config"}
+accounts=${6:-"site/accounts"}
 
-mv -f ${source} ${source}_`date -u --iso-8601=seconds`
-ln -s ${target} ${source}
+working_dir = dirname ${source_path}
+source_folder = basename ${source_path}
+target_folder = `basename ${target_path}`"/${subpath}"
+
+cd working_dir
+
+rsync -a "${source_folder}/${content}" "${target_folder}/${content}"
+rsync -a "${source_folder}/${config}" "${target_folder}/${config}"
+rsync -a "${source_folder}/${accounts}" "${target_folder}/${accounts}"
+
+mv -f ${source_folder} ${source_folder}_`date -u --iso-8601=seconds`
+ln -s ${target_folder} ${source_folder}
