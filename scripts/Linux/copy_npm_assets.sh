1f515a534936333a9da2fdd14daf7d3739c641a5
Marco Köllner <it@mk-swe.de>
2021-01-10 18:06:07 +0100

    Continious delivery for Staging & Production (#68) (#69)
    * change docker build script
    
    * Docker file for deployment
    
    * change action branch for test
    
    * add cache
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * just check submodules
    
    * add submodules
    
    * change checkout
    
    * add token
    
    * Create rsync.ignore
    
    * Update deployToServer.yml
    
    * Bugfix
    
    * prepare files for scp
    
    * second bugfix
    
    * fix typo
    
    * try env var
    
    * change to rsync
    
    * bugfix mkdir
    
    * cleanup
    
    * bugfix cleanup
    
    * bugfix cleanup
    
    * Update deployToServer.yml
    
    * Update deployToServer.yml
    
    * Update deployToServer.yml
    
    * Update deployToServer.yml
    
    * Update deployToServer.yml
    
    * Update deployToServer.yml
    
    * Update deployToServer.yml
    
    * Update deployToServer.yml
    
    * Update deployToServer.yml
    
    * bugfix cleanup
    
    * bugfix cleanup
    
    * bugfix cleanup
    
    * bugfix cleanup
    
    * bugfix cleanup
    
    * bugfix cleanup
    
    * bugfix cleanup
    
    * bugfix cleanup
    
    * bugfix cleanup
    
    * bugfix cleanup
    
    * bugfix cleanup
    
    * bugfix linux Script
    
    * bugfix deploy
    
    * bugfix deploy
    
    * bugfix deploy
    
    * Updated from server
    
    * Checked in Content update
    
    * Add missing pdf in content
    
    * Update Modules
    
    * rework the deployment
    
    * fix syntax error in yml
    
    * update packages to fix composer error
    
    * correct environment
    
    * correct the publish
    
    * add artifacts
    
    * fix yml error
    
    * fix yml error
    
    * fix wrong artifacts input folder
    
    * copy build to folder
    
    * fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * another fix in yml
    
    * change to different action
    
    * another commit from sourcetree
    
    * add blue green deployment
    
    * bugfix deploy error
    
    * bugfix publish
    
    * bugfix publish
    
    * bugfix publish
    
    * bugfix publish
    
    * bugfix publish
    
    * bugfix 100 for publish
    
    * bugfix 101 for publish
    
    * bugfix 102 for publish
    
    * bugfix 103 for publish
    
    * bugfix 104 for publish
    
    * change di deploy to staging
    
    * add production environment

--- scripts/Linux/copy_npm_assets.sh
+++ scripts/Linux/copy_npm_assets.sh
@@ -8,7 +8,7 @@ project_path=$(realpath ${script_path}/../../)
 mkdir -p ${project_path}/assets/vendors/bootstrap && rsync -rv ${project_path}/node_modules/bootstrap/dist/ ${project_path}/assets/vendors/bootstrap
 mkdir -p ${project_path}/assets/vendors/jquery && rsync -rv ${project_path}/node_modules/jquery/dist/ ${project_path}/assets/vendors/jquery
 mkdir -p ${project_path}/assets/vendors/popper && rsync -rv ${project_path}/node_modules/popper.js/dist/ ${project_path}/assets/vendors/popper
-mkdir -p ${project_path}/assets/vendors/fontawesome-free && rsync -rv ${project_path}/node_modules/@fortawesome/fontawesome-free/ ${project_path}/assets/vendors/fontawesome-free
+mkdir -p ${project_path}/assets/vendors/fontawesome-free && rsync -rv --include=css/*** --include=webfonts/*** --exclude=* ${project_path}/node_modules/@fortawesome/fontawesome-free/ ${project_path}/assets/vendors/fontawesome-free
 mkdir -p ${project_path}/assets/vendors/cookieconsent && rsync -rv ${project_path}/node_modules/cookieconsent/src/ ${project_path}/assets/vendors/cookieconsent
 mkdir -p ${project_path}/assets/vendors/photoswipe && rsync -rv ${project_path}/node_modules/photoswipe/dist/ ${project_path}/assets/vendors/photoswipe
 mkdir -p ${project_path}/assets/vendors/waypoints && rsync -rv ${project_path}/node_modules/waypoints/lib/ ${project_path}/assets/vendors/waypoints
