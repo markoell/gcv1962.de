5f6bb973ae8e87d6dbbe12d50871e33c2f298dd0
Marco Koellner <it@mk-swe.de>
2021-04-19 22:00:05 +0200

    fixes

--- site/snippets/chronicle/previous-couples.php
+++ site/snippets/chronicle/previous-couples.php
@@ -1,7 +1,7 @@
 <?php if($data != null): ?>
   <?php foreach($data as $item): ?>
     <div class="col mb-4">
-      <?php snippet('chronik/royal-couple', array('data' => $item)); ?>
+      <?php snippet('chronicle/royal-couple', array('data' => $item)); ?>
     </div>
   <?php endforeach ?>
 <?php endif ?>
--- site/snippets/chronicle/second-third-couple.php
+++ site/snippets/chronicle/second-third-couple.php
@@ -1,10 +1,10 @@
 <div class="col-sm-12 mb-4">
   <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-1 h-100">
     <div class="col mb-4 mb-sm-0 mb-lg-4">
-      <?php snippet('chronik/royal-couple', array('data' => $secondCouple)) ?>
+      <?php snippet('chronicle/royal-couple', array('data' => $secondCouple)) ?>
     </div>
     <div class="col align-self-end">
-      <?php snippet('chronik/royal-couple', array('data' => $thirdCouple)) ?>
+      <?php snippet('chronicle/royal-couple', array('data' => $thirdCouple)) ?>
     </div>
   </div>
 </div>
