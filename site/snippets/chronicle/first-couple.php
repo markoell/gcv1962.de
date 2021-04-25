bf75f24bcde716b7bda65e4c127f703aa139fba2
Marco Koellner <it@mk-swe.de>
2021-04-25 17:06:21 +0200

    add civil names in royal couples

--- site/snippets/chronicle/first-couple.php
+++
@@ -1,3 +0,0 @@
-<div class="col-sm-12 col-lg-8 mb-4">
-  <?php snippet('chronicle/royal-couple', array('data' => $firstCouple)) ?>
-</div>
--- site/templates/royal-couples.php
+++ site/templates/royal-couples.php
@@ -1,11 +1,11 @@
-<?php snippet('header', array('sticky' => true)) ?>
+<?php snippet('header', ['sticky' => true, 'css' => ["gcv-feature.css"]]) ?>
 
 <div class="container pt-5">
   <h1><?php echo $page->title()->html() ?></h1>
   <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
-    <?php snippet('chronicle/first-couple', ['data' => $firstCouple]) ?>
-    <?php snippet('chronicle/second-third-couple', ['secondCouple' => $secondCouple, 'thirdCouple' => $thirdCouple]) ?>
-    <?php snippet('chronik/previous-couples', ['data' => $previousCouples]) ?>
+    <?php snippet('chronicles/first-couple', ['data' => $firstCouple]) ?>
+    <?php snippet('chronicles/second-third-couple', ['secondCouple' => $secondCouple, 'thirdCouple' => $thirdCouple]) ?>
+    <?php snippet('chronicles/previous-couples', ['data' => $previousCouples]) ?>
   </div>
 </div>
 
