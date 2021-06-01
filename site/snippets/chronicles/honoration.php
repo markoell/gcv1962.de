d46c12822a63918b1e16cb415d8ef5f85692ff64
Marco Koellner <it@mk-swe.de>
2021-06-01 16:14:00 +0200

    first adaptions in snippet

--- site/snippets/chronicles/honoration.php
+++ site/snippets/chronicles/honoration.php
@@ -7,6 +7,15 @@
       <div class="card-body">
         <h5 class="card-title"><?= $data->title()->h() ?></h5>
         <p class="card-text"><small class="text-muted"><?= $data->description()->kt() ?></small></p>
+        <?php 
+          $items = $data->valued_members()->toStructure();
+          foreach (group_by('year', $items) as $item): ?>
+            <h2><?= $item->year()->html() ?></h2>
+            <?php foreach ($item->images()->toFiles() as $image): ?>
+              <img src="<?= $image->crop(400)->url() ?>">
+            <?php endforeach ?>
+            <p><?= $item->price() ?></p>
+          <?php endforeach ?>
       </div>
     </div>
   </div>
