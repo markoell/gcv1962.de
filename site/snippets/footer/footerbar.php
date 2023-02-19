e7205fe2b48f0a45da7a1b329e7088a284e1c2f3
Marco Köllner <it@mk-swe.de>
2023-02-19 18:34:52 +0100

    remove obsolete photoswipePswpElement

--- site/snippets/footer/footerbar.php
+++ site/snippets/footer/footerbar.php
@@ -1,30 +1,29 @@
-    <footer class="footer bg-dark mt-auto py-3">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-5">
+<footer class="footer bg-dark mt-auto py-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-5">
 
-            <ul class="nav">
-              <?php foreach (pages(['footer'])->children()->listed() as $item): ?>
-              <li class="nav-item">
-                <a class="nav-link text-white"
-                  href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
-              </li>
-              <?php endforeach ?>
-              <li>
-                <a class="nav-link text-white"
-                  href="mailto:geschaeftsstelle@gcv1962.de?subject=Anfrage">Kontakt</a>
-              </li>
-            </ul>
+        <ul class="nav">
+          <?php foreach (pages(['footer'])->children()->listed() as $item): ?>
+          <li class="nav-item">
+            <a class="nav-link text-white"
+              href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
+          </li>
+          <?php endforeach ?>
+          <li>
+            <a class="nav-link text-white"
+              href="mailto:geschaeftsstelle@gcv1962.de?subject=Anfrage">Kontakt</a>
+          </li>
+        </ul>
 
-          </div>
-          <div class="col-lg my-auto">
-            <?php snippet('footer/sozialMediaButtons') ?>
-          </div>
-          <div class="col-lg-5 my-auto">
-            <span class="text-white">Copyright &copy; <?= $site->copyright() ?></span>
-          </div>
-
-        </div>
       </div>
-    </footer>
-    <?= snippet('footer/photoswipePswpElement') ?>
+      <div class="col-lg my-auto">
+        <?php snippet('footer/sozialMediaButtons') ?>
+      </div>
+      <div class="col-lg-5 my-auto">
+        <span class="text-white">Copyright &copy; <?= $site->copyright() ?></span>
+      </div>
+
+    </div>
+  </div>
+</footer>
